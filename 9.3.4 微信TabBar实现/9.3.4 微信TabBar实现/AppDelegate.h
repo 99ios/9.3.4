317613938a0d7bc3fc3570fc82e4071d47f1a52c
test <422538722@qq.com>
2017-04-16 15:20:21 +0800

    9.3.4
     微信TabBar实现

---
+++ 9.3.4 微信TabBar实现/9.3.4 微信TabBar实现/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  9.3.4 微信TabBar实现
+//
+//  Created by 李维佳 on 2017/4/4.
+//  Copyright © 2017年 Liz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
